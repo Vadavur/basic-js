58107ca3f89c405640cb914cfd8af67ffafd7b18
Vadavur <vadavur@gmail.com>
2021-03-25 01:47:31 +0300

    init: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,44 @@
+const { prototype } = require("../extensions/custom-error");
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: "",
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain[this.chain.length - 1] != "~"
+      ? this.chain.split("~~").length
+      : this.split.split("~~").length - 1;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) value = "( )";
+    else value = `( ${value} )`;
+    this.chain += `${value}~~`;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position)) {
+      this.chain = "";
+      throw new Error("Not an integer, man!!!");
+    }
+    let arr = this.chain.split("~~");
+    if (position > arr.length - 1 || position < 1) {
+      this.chain = "";
+      throw new Error("OUT OF RAnGE!");
+    }
+    arr.splice(position - 1, 1);
+    this.chain = arr.join("~~");
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let reversedChain = this.chain.split("~~").reverse().join("~~");
+    this.chain =
+      reversedChain[0] != "~" ? reversedChain : reversedChain.slice(2) + "~~";
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let chain = this.chain.slice(0, -2);
+    this.chain = "";
+    return chain;
+  },
 };
 
 module.exports = chainMaker;
